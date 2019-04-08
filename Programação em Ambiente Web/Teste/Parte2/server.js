92c66f82df07b92537d0ee6994ead2c90e3fb30a
ricardof18 <44167524+ricardof18@users.noreply.github.com>
2019-04-08 23:26:57 +0100

    comiit 2

--- Programação em Ambiente Web/Teste/Parte2/server.js
+++ Programação em Ambiente Web/Teste/Parte2/server.js
@@ -1,27 +1,47 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+var formidable=require('formidable');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
 
-  //TODO  
+  //TODO
+  var e="/home"
+  var q=url.parse(req.url,true);
+  var q1=q.pathname;
+  if(req.url=='/home'){
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
 
-var q=url.parse(req.url,true);
-var filename="."+q.pathname;7
+    });
+  }else if(req.url=='/form'){
 
-fs.readFile('index.html',function(err,data){
-	if(err){
-	res.writeHead(404,{'Content-Type':'text/html'});
-	return res.end("404 Not Found!");
-}
-res.writeHead(200,{'Content-Type':'text/html'});
-res.write(data);
-return res.end();
-});
-  
+    var form = new formidable.IncomingForm();
+
+    form.parse(req,function(err,fields,files){
+      var data = "nome:"+ fields.pnome+ ",idade:" +fields.idade;
+      var file = __dirname + "/" + fields.pnome +".txt"
+
+      fs.writeFile(file,data, function (err){
+        if(err){
+          console.log(err);
+        }else{
+          res.end("sucesso " +fields.pnome + "!");
+        }
+      });
+
+    });
+  }
+
+  else{
+    res.writeHead(404,{'Content-Type':'text/html'});
+    return res.end("404 Not Found!");
+};
 });
 
 server.listen(port, hostname, () => {
