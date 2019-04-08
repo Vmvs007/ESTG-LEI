e50f4f3d5054f4d9bc65958449d08e9269342e62
Ricardo <Dumbrica@hotmail.com>
2019-04-08 11:43:47 +0100

    PP_8170278

---
+++ Paradigmas de Programação/8170278/PP_FP05/src/pp_fp05/PP_FP05.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp_fp05;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class PP_FP05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
